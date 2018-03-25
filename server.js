a4b7642c6f964433fd5c3c3688312aaaad5cfb44
Shane Harris <shane@xactaccounts.co.uk>
2018-03-25 12:56:45 +0100

    minor fix

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ Server.prototype = {
         this.savePortals();
     },
     removePortal:function(msg,portal_room){
-        portal_room.children.splice(msg.index,1);
+        portal_room.children.splice(msg.portalNumber,1);
         this.savePortals();
     },
     savePortals:function(){
